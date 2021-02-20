a0c6898b0843d79fcec36a273ff5e5eb71dd6f38
Natsu29 <natsupersonal@gmail.com>
2021-02-20 17:38:21 +0600

    The first initial commit

---
+++ cpp/temp.cpp
@@ -0,0 +1,28 @@
+/* Title: the title of the project goes here.
+ * Author: Natsu29
+ * Date:xx/xx/xx
+ * sign: @ashh
+ */
+#include <bits/stdc++.h>
+#define nl "\n"
+#define pb push_back
+#define eb emplace_back
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpi;
+typedef vector<vector<int, int>> vvi;
+
+
+int main(int arg, char ** argv){
+	ios::sync_with_stdio(false);
+
+	//this is the main template
+
+
+	return 0;
+
+};
+
+
